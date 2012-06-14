cb6414442d5858f7b909422f9f6f90ec294c7486
Michael Stransky <Michael.Stransky@g-node.org>
2012-06-14 15:46:28 +0200

    working import with new orm

--- usecases/import.py
+++ usecases/import.py
@@ -8,6 +8,9 @@ import datajongleur
 import time
 import morphjongleur.util.parser.swc
 from morphjongleur.orm.datajongleur.morphology import *
+from morphjongleur.orm.datajongleur.neuron_single_cell_experiment import *
+from morphjongleur.orm.datajongleur.clamp import *
+from morphjongleur.orm.datajongleur.neuron import *
 
 session = datajongleur.get_session()
 
@@ -17,6 +20,7 @@ def import_swcs(swcs):
 
 def import_swc(swc):
     m_swc   = Morphology.swc_parse(swc)
+    #m_swc   = Morphology.specify(m_swc)
 
     print("uploading data according to '%s'." %(swc)) ,
     start_time = time.time()
@@ -24,10 +28,10 @@ def import_swc(swc):
     end_time = time.time()
     print("in %s seconds." %(end_time-start_time))
     
-    print m_swc.morphology_key
+    print m_swc.uuid
     uuid = m_swc.uuid
-    m_swc2 = InfoQuantity.load(uuid)
-    print( m_swc2 )
+    m_swc2 = Identity.load(uuid)
+    print( type(m_swc2), m_swc2 )
 
     return m_swc
 
@@ -35,13 +39,5 @@ def import_swc(swc):
 if __name__ == '__main__':
     import sqlalchemy
     print sqlalchemy.__version__
-    #root_dir = '/home/philipp/Repositories/G-Node/mitsubachi/'
-    root_dir = '/home/stransky/git/mitsubachi/'
-    #root_dir = '../../../'
-    import_swcs([
-#         root_dir + 'data/test.swc', 
-         root_dir + 'data/H060602DB_10_2_zentai_.swc', 
-         root_dir + 'data/H060602VB_10_2_zentai_.swc', 
-         root_dir + 'data/H060607DB_10_2(zentai).swc', 
-         root_dir + 'data/H060607VB_10_2(zentai).swc'
-    ])
+    assert sqlalchemy.__version__ == "0.7.4"    #sudo easy_install "sqlalchemy==0.7.4"
+    import_swcs(['../data/test.swc'])
